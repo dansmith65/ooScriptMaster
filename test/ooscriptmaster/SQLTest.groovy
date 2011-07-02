0f231e194641b325fc346b502c19d0cdf99abcd7
Dan Smith <dansmith65@gmail.com>
2011-07-02 12:40:30 -0700

    remove sqlField and sqlTable methods in favor of using Custom Functions that perform the same task

--- test/ooscriptmaster/SQLTest.groovy
+++ test/ooscriptmaster/SQLTest.groovy
@@ -23,58 +23,5 @@ public class SQLTest {
     }
     */
 
-	//sqlField
-	@Test(expected = ValidationException.class)
-	public void sqlField_empty_ValidationException() {
-		String field = ''
-		SQL.sqlField(field)
-	}
-
-	@Test(expected = ValidationException.class)
-	public void sqlField_invalid_ValidationException() {
-		String field = 'a::b::c'
-		SQL.sqlField(field)
-	}
-
-	@Test public void sqlField_fieldNameOnly_matchString() {
-		String field = 'fieldName'
-		String expected = '"fieldName"'
-		def result = SQL.sqlField(field)
-		assertEquals(expected, result)
-	}
-
-	@Test public void sqlField_tableAndFieldName_matchString() {
-		String field = 'table::field Name'
-		String expected = '"table"."field Name"'
-		def result = SQL.sqlField(field)
-		assertEquals(expected, result)
-	}
-
-	//sqlTable
-	@Test(expected = ValidationException.class)
-	public void sqlTable_empty_ValidationException() {
-		String table = ''
-		SQL.sqlTable(table)
-	}
-
-	@Test(expected = ValidationException.class)
-	public void sqlTable_invalid_ValidationException() {
-		String table = 'a::b::c'
-		SQL.sqlTable(table)
-	}
-
-	@Test public void sqlTable_tableNameOnly_matchString() {
-		String table = 'tableName'
-		String expected = '"tableName"'
-		def result = SQL.sqlTable(table)
-		assertEquals(expected, result)
-	}
-
-	@Test public void sqlTable_tableAndTableName_matchString() {
-		String table = 'table Name::field Name'
-		String expected = '"table Name"'
-		def result = SQL.sqlTable(table)
-		assertEquals(expected, result)
-	}
 
 }
